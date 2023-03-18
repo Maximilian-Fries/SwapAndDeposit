8421dee8764ba2fd5230fb4e942fcb3822290163
Maximilian-Fries <maxif98@gmx.de>
2023-03-18 09:38:25 +0100

    remove legacy code

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,19 +1,10 @@
-from solcx import compile_standard, install_solc
-import json
-from web3 import Web3
-import os
-from dotenv import load_dotenv
-from brownie import accounts, config, SwapAndDeposit
+from brownie import SwapAndDeposit
 from scripts.helpers import get_account
 
 
 def deploy_swapAnddeposit():
     account = get_account()
-    swap_and_deposit = SwapAndDeposit.deploy({"from": account})
-
-
-def this_is_a_test():
-    print("success")
+    SwapAndDeposit.deploy({"from": account})
 
 
 def main():
--- tests/my_tests.py
+++
@@ -1,9 +0,0 @@
-
-# Things already tested
-
-# tokenIn = ETH, tokenOut = DAI (ERC20)
-
-
-# TODO
-# tokenIn = ERC20, tokenOut = ETH
-# There is no cWETH, so case where tokenout = weth can be ignored. 
