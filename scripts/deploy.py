997f9f02c04930b4badf444813f9eae5b44b3a81
Maximilian-Fries <49775914+Maximilian-Fries@users.noreply.github.com>
2024-08-27 20:15:55 +0200

    test commit

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,7 @@
 from brownie import SwapAndDeposit
 from scripts.helpers import get_account
 
-
+# dummy comment
 def deploy_swapAnddeposit():
     account = get_account()
     SwapAndDeposit.deploy({"from": account})
